7256c3ed1a43f8bf576ed4d0ec35cd56d27ee146
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-06-11 17:30:02 +0900

    feat: Respond

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/yamaryu0508/alexa-skills-kit-color-expert-go/alexa"
 )
 
-type Response struct {
-	Message string `json:"message"`
-}
 
-func Handler() (Response, error) {
-	return Response{
-		Message: "Go Serverless v1.0! Your function executed successfully!",
-	}, nil
+func Handler(event alexa.Request) (alexa.Response, error) {
+	fmt.Println(event)
+	sessionAttributes := make(map[string]interface{})
+	return alexa.BuildResponse(
+		sessionAttributes,
+		alexa.BuildSpeechletResponse("test", "テストです", "テストです", false),
+		), nil
 }
 
 func main() {
